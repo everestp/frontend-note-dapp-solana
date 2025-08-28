d8a06ee573d43327f1d5f9dbb400f2d8e14f005b
Everest Paudel <paudeleverest09@gmail.com>
2025-08-28 21:31:59 +0545

    feat: add note creation functionality and improve error handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { AnchorProvider, Program } from "@project-serum/anchor";
 import { WalletReadyState } from "@solana/wallet-adapter-base";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
-import { PublicKey } from "@solana/web3.js";
+import { PublicKey, SystemProgram } from "@solana/web3.js";
 import { ClientPageRoot } from "next/dist/client/components/client-page";
+import { setLazyProp } from "next/dist/server/api-utils";
 import { getWaitUntilPromiseFromEvent } from "next/dist/server/web/spec-extension/fetch-event";
 import Image from "next/image";
-import { use, useState } from "react";
+import {  useState } from "react";
   
 const PROGRAM_ID = new PublicKey("EuXGJJMr69HGJycN5TMNF7ek3d1fxMSKMpqFbWqMmeH7")
 const IDL ={"version":"0.1.0","name":"note_dapp","constants":[{"name":"NOTE_SEED","type":"bytes","value":"[110, 111, 116, 101]"},{"name":"POST_SEED","type":"bytes","value":"[112, 111, 115, 116]"}],"instructions":[{"name":"createNote","accounts":[{"name":"noteAccount","isMut":true,"isSigner":false},{"name":"author","isMut":true,"isSigner":true},{"name":"systemProgram","isMut":false,"isSigner":false}],"args":[{"name":"title","type":"string"},{"name":"content","type":"string"}]},{"name":"updateNote","accounts":[{"name":"noteAccount","isMut":true,"isSigner":false},{"name":"author","isMut":false,"isSigner":true}],"args":[{"name":"updateContent","type":"string"}]},{"name":"deleteNote","accounts":[{"name":"noteAccount","isMut":true,"isSigner":false},{"name":"author","isMut":true,"isSigner":true}],"args":[]}],"accounts":[{"name":"Note","type":{"kind":"struct","fields":[{"name":"author","type":"publicKey"},{"name":"title","type":"string"},{"name":"content","type":"string"},{"name":"createdAt","type":"i64"},{"name":"lastUpdate","type":"i64"}]}}],"errors":[{"code":6000,"name":"TitleTooLong","msg":"Title cannot be longer than 100 chars"},{"code":6001,"name":"ContentTooLong","msg":"Content cannot be longer than 1000 chars"},{"code":6002,"name":"TitleEmpty","msg":"Title cannot be empty"},{"code":6003,"name":"ContentEmpty","msg":"Content cannot be empty"},{"code":6004,"name":"Unauthorized","msg":"Unauthorized"}]}
@@ -17,13 +18,31 @@ export default function Home() {
   const wallet = useWallet();
 const [notes ,setNotes] =  useState<any []>([])
 const [loading ,setLoading]=  useState(false)
-
+const [message ,setMessage] = useState("")
+const [title ,setTitle] = useState("")
+const [content ,setContent] = useState("")
+const [editTile ,setEditTitle] = useState("")
+const [editContent ,setEditContent] = useState("")
   const  getProgram = ()=>{
     if(!wallet.publicKey || !wallet.signTransaction) return null;
     const provider = new AnchorProvider(connection , wallet as any ,{})
     return new Program(IDL as any , PROGRAM_ID ,provider)
   }
 
+
+  const getNoteAddress =   (title:String)=>{
+  if(!wallet.publicKey || !wallet.signTransaction) return null;
+const [noteAddress] =   PublicKey.findProgramAddressSync([
+    
+    Buffer.from("note"),
+    wallet.publicKey.toBuffer(),
+    Buffer.from(title)
+  
+  
+  ],PROGRAM_ID)
+
+  return noteAddress;
+  }
   //load the note
 
   const loadNotes = async () =>{
@@ -45,14 +64,108 @@ const [loading ,setLoading]=  useState(false)
 
         ]);
         setNotes(notes)
+        setMessage("")
         console.log(notes)
      } catch (error) {
-      
+      setMessage("Error Loading the notes")
      }
      setLoading(false)
   }
   //create the note
+const  createNote = async () =>{
+  if(!title.trim() || !title.trim()){
+    setMessage("Please fill the title and the content")
+  }
+
+if(title.length >100){
+  setMessage("Title is too long :100 char Max")
+}
+if(content.length >100){
+  setMessage("Content is too long :1000 char Max")
+}
+setLoading(true)
+try {
+  const program = getProgram();
+  if(!program) return ;
+  const noteAddress = getNoteAddress(title)
+  if(!noteAddress) return
+  await program.methods.createNote(title ,content).accounts({
+note_account:noteAddress,
+    author :wallet.publicKey,
+    systemProgram :SystemProgram.programId
+
+  }
+    
+  ).rpc()
+  setMessage("Note Create Successfully")
+  setLoading(false)
+  setTitle("")
+  setContent("")
+} catch (error) {
+  console.log("Error creating note" ,error)
+  setMessage("Error creating note")
+  await loadNotes()
+}
+
+setLoading(false)
+
+}
+
+//upate note
+const updateNote  = async (note:any) =>{
+  if(!editContent.trim()){
+    setMessage("Content Cannot be Empty")
+    return;
+  }
+  if(editContent.length > 1000){
+      setMessage("Content tO long Max: 1000 chars")
+    return;
+  }
+setLoading(true)
+  try {
+   const program = getProgram();
+  if(!program) return ;
+  const noteAddress = getNoteAddress(note.account.title)
+  if(!noteAddress) return
+  await program.methods.updateNote(editContent).accounts({
+    note_accont:noteAddress,
+    author:wallet.publicKey
+  }).rpc()
+  setMessage("Note update sucessfully")
+  setEditContent("")
+  setEditTitle("")
+  await loadNotes()
+  } catch (error) {
+    console.log("Error updating notes",error)
+    setMessage("Erro updating notes")
+  }
+  setLoading(false)
+}
+
   //delete the note
+
+  const deleteNote = async (note:any)=>{
+setLoading(true)
+try {
+     const program = getProgram();
+  if(!program) return ;
+  const noteAddress = getNoteAddress(note.account.title)
+  if(!noteAddress) return
+  await program.methods.deleteNote().accounts({
+    note_accont:noteAddress,
+    author:wallet.publicKey
+  }).rpc();
+
+setMessage("Note delete successfully");
+await loadNotes()
+
+
+} catch (error) {
+ console.log("Error deleting note",error)
+ setMessage("Error deleting the note")
+}
+setLoading(false)
+  }
 
   return (
     <div>
