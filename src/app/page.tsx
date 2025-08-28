8012b36b9abd1b38f8a78fe96fe4c1c78ef0584b
Everest Paudel <paudeleverest09@gmail.com>
2025-08-28 22:48:32 +0545

    feat: enhance note creation UI and add wallet connection handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
+"use client"
+
 import { AnchorProvider, Program } from "@project-serum/anchor";
-import { WalletReadyState } from "@solana/wallet-adapter-base";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { PublicKey, SystemProgram } from "@solana/web3.js";
-import { ClientPageRoot } from "next/dist/client/components/client-page";
-import { setLazyProp } from "next/dist/server/api-utils";
-import { getWaitUntilPromiseFromEvent } from "next/dist/server/web/spec-extension/fetch-event";
-import Image from "next/image";
-import {  useState } from "react";
-  
+
+import { useState } from "react";
+import { useEffect } from "react";
+
 const PROGRAM_ID = new PublicKey("EuXGJJMr69HGJycN5TMNF7ek3d1fxMSKMpqFbWqMmeH7")
-const IDL ={
+const IDL = {
   "version": "0.1.0",
   "name": "note_dapp",
   "constants": [
@@ -83,163 +82,231 @@ const IDL ={
 
 
 export default function Home() {
-  const {connection} = useConnection();
+  const { connection } = useConnection();
   const wallet = useWallet();
-const [notes ,setNotes] =  useState<any []>([])
-const [loading ,setLoading]=  useState(false)
-const [message ,setMessage] = useState("")
-const [title ,setTitle] = useState("")
-const [content ,setContent] = useState("")
-const [editTile ,setEditTitle] = useState("")
-const [editContent ,setEditContent] = useState("")
-  const  getProgram = ()=>{
-    if(!wallet.publicKey || !wallet.signTransaction) return null;
-    const provider = new AnchorProvider(connection , wallet as any ,{})
-    return new Program(IDL as any , PROGRAM_ID ,provider)
+  const [notes, setNotes] = useState<any[]>([])
+  const [loading, setLoading] = useState(false)
+  const [message, setMessage] = useState("")
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+  const [editTile, setEditTitle] = useState("")
+  const [editContent, setEditContent] = useState("")
+  const getProgram = () => {
+    if (!wallet.publicKey || !wallet.signTransaction) return null;
+    const provider = new AnchorProvider(connection, wallet as any, {})
+    return new Program(IDL as any, PROGRAM_ID, provider)
   }
 
 
-  const getNoteAddress =   (title:String)=>{
-  if(!wallet.publicKey || !wallet.signTransaction) return null;
-const [noteAddress] =   PublicKey.findProgramAddressSync([
-    
-    Buffer.from("note"),
-    wallet.publicKey.toBuffer(),
-    Buffer.from(title)
-  
-  
-  ],PROGRAM_ID)
+  const getNoteAddress = (title: String) => {
+    if (!wallet.publicKey || !wallet.signTransaction) return null;
+    const [noteAddress] = PublicKey.findProgramAddressSync([
 
-  return noteAddress;
+      Buffer.from("note"),
+      wallet.publicKey.toBuffer(),
+      Buffer.from(title)
+
+
+    ], PROGRAM_ID)
+
+    return noteAddress;
   }
   //load the note
 
-  const loadNotes = async () =>{
+  const loadNotes = async () => {
     if (!wallet.publicKey) return;
-     try {
-      const  program = getProgram()
-      if(!program) return;
-  setLoading(true)
-      const notes = await program.account.note_account.all(
+    try {
+      const program = getProgram()
+      if (!program) return;
+      setLoading(true)
+      const notes = await program.account.note.all(
         [
           {
-            memcmp :{
-              offset : 8 , //account:Note:{author ,title ,con}
-              bytes :wallet.publicKey.toBase58(),
+            memcmp: {
+              offset: 8, //account:Note:{author ,title ,con}
+              bytes: wallet.publicKey.toBase58(),
 
 
             }
           }
 
         ]);
-        setNotes(notes)
-        setMessage("")
-        console.log(notes)
-     } catch (error) {
+      setNotes(notes)
+      setMessage("")
+      console.log(notes)
+    } catch (error) {
       setMessage("Error Loading the notes")
-     }
-     setLoading(false)
+    }
+    setLoading(false)
   }
   //create the note
-const  createNote = async () =>{
-  if(!title.trim() || !title.trim()){
-    setMessage("Please fill the title and the content")
+  const createNote = async () => {
+    if (!title.trim() || !title.trim()) {
+      setMessage("Please fill the title and the content")
+    }
+
+    if (title.length > 100) {
+      setMessage("Title is too long :100 char Max")
+    }
+    if (content.length > 100) {
+      setMessage("Content is too long :1000 char Max")
+    }
+    setLoading(true)
+    try {
+      const program = getProgram();
+      if (!program) return;
+      const noteAddress = getNoteAddress(title)
+      if (!noteAddress) return
+      await program.methods.createNote(title, content).accounts({
+        noteAccount: noteAddress,
+        author: wallet.publicKey,
+        systemProgram: SystemProgram.programId
+
+      }
+
+      ).rpc()
+      setMessage("Note Create Successfully")
+      setLoading(false)
+      setTitle("")
+      setContent("")
+    } catch (error) {
+      console.log("Error creating note", error)
+      setMessage("Error creating note")
+      await loadNotes()
+    }
+
+    setLoading(false)
+
   }
 
-if(title.length >100){
-  setMessage("Title is too long :100 char Max")
-}
-if(content.length >100){
-  setMessage("Content is too long :1000 char Max")
-}
-setLoading(true)
-try {
-  const program = getProgram();
-  if(!program) return ;
-  const noteAddress = getNoteAddress(title)
-  if(!noteAddress) return
-  await program.methods.createNote(title ,content).accounts({
-note_account:noteAddress,
-    author :wallet.publicKey,
-    systemProgram :SystemProgram.programId
-
-  }
-    
-  ).rpc()
-  setMessage("Note Create Successfully")
-  setLoading(false)
-  setTitle("")
-  setContent("")
-} catch (error) {
-  console.log("Error creating note" ,error)
-  setMessage("Error creating note")
-  await loadNotes()
-}
-
-setLoading(false)
-
-}
-
-//upate note
-const updateNote  = async (note:any) =>{
-  if(!editContent.trim()){
-    setMessage("Content Cannot be Empty")
-    return;
-  }
-  if(editContent.length > 1000){
+  //upate note
+  const updateNote = async (note: any) => {
+    if (!editContent.trim()) {
+      setMessage("Content Cannot be Empty")
+      return;
+    }
+    if (editContent.length > 1000) {
       setMessage("Content tO long Max: 1000 chars")
-    return;
+      return;
+    }
+    setLoading(true)
+    try {
+      const program = getProgram();
+      if (!program) return;
+      const noteAddress = getNoteAddress(note.account.title)
+      if (!noteAddress) return
+      await program.methods.updateNote(editContent).accounts({
+        note: noteAddress,
+        author: wallet.publicKey
+      }).rpc()
+      setMessage("Note update sucessfully")
+      setEditContent("")
+      setEditTitle("")
+      await loadNotes()
+    } catch (error) {
+      console.log("Error updating notes", error)
+      setMessage("Erro updating notes")
+    }
+    setLoading(false)
   }
-setLoading(true)
-  try {
-   const program = getProgram();
-  if(!program) return ;
-  const noteAddress = getNoteAddress(note.account.title)
-  if(!noteAddress) return
-  await program.methods.updateNote(editContent).accounts({
-    note_accont:noteAddress,
-    author:wallet.publicKey
-  }).rpc()
-  setMessage("Note update sucessfully")
-  setEditContent("")
-  setEditTitle("")
-  await loadNotes()
-  } catch (error) {
-    console.log("Error updating notes",error)
-    setMessage("Erro updating notes")
-  }
-  setLoading(false)
-}
 
   //delete the note
 
-  const deleteNote = async (note:any)=>{
-setLoading(true)
-try {
-     const program = getProgram();
-  if(!program) return ;
-  const noteAddress = getNoteAddress(note.account.title)
-  if(!noteAddress) return
-  await program.methods.deleteNote().accounts({
-    note_accont:noteAddress,
-    author:wallet.publicKey
-  }).rpc();
+  const deleteNote = async (note: any) => {
+    setLoading(true)
+    try {
+      const program = getProgram();
+      if (!program) return;
+      const noteAddress = getNoteAddress(note.account.title)
+      if (!noteAddress) return
+      await program.methods.deleteNote().accounts({
+        note: noteAddress,
+        author: wallet.publicKey
+      }).rpc();
 
-setMessage("Note delete successfully");
-await loadNotes()
+      setMessage("Note delete successfully");
+      await loadNotes()
 
 
-} catch (error) {
- console.log("Error deleting note",error)
- setMessage("Error deleting the note")
-}
-setLoading(false)
+    } catch (error) {
+      console.log("Error deleting note", error)
+      setMessage("Error deleting the note")
+    }
+    setLoading(false)
+  }
+  useEffect(() => {
+    if (wallet.connected) {
+      loadNotes()
+    }
+  }, [wallet.connected]);
+
+  if (!wallet.connected) {
+    return <div className="text-gray-700">Wallet is not connected please Connect your wallet </div>
   }
 
   return (
-    <div>
-<h1>Note Dapp</h1>
-      
+
+    <div className="text-gray-700">
+      <div>
+        <h2 className="text-2xl mb-6">Create New Note</h2>
+         {/* for  title */}
+        <div className="mb-4">
+                  <label className="text-sm block font-medium">
+  Title (
+  {title.length > 100? <div className="inline text-red-500">Limit Exceed</div> : `${title.length.toLocaleString()}/100`}
+  
+  )
+</label>
+<input 
+type="text"
+ name="title"
+  value={title} 
+  placeholder="Title here ..."
+  onChange={(e)=>setTitle(e.target.value)}
+  className="border-2 border-gray-300 rounded-lg p-2 w-full"
+
+/>
+        </div>
+
+        {/* for content */}
+                <div className="mb-4">
+                  <label className="text-sm block font-medium">
+  Content (
+  {content.length > 1000? <div className="inline text-red-500">Limit Exceed</div> : `${content.length.toLocaleString()}/100`}
+  
+  )
+</label>
+
+<textarea 
+maxLength={1000}
+ name="content"
+  value={content}
+  placeholder="Content Here ..."
+  rows={5} 
+  onChange={(e)=>setContent(e.target.value)}
+  className=" border-2 border-gray-300 rounded-lg p-2 w-full"
+
+/>
+        </div>
+        <button
+        className="w-full bg-blue-500 rounded-lg text-white font-bold px-4 py-2  cursor-ponter disabled:bg-blue-300 disabled:cursor-not-allowed"
+        disabled={loading || !title.trim() ||!content.trim() }
+        onClick={createNote}
+        >
+          
+          {loading ?"Creating note ..": "Create Note"}
+        
+        </button>
+      </div>
+      <div>
+        {notes?.map((note: any, index: number) => {
+          return <div>
+<div>{JSON.stringify(note)}</div>
+          </div>
+        })}
+      </div>
     </div>
+
+
   );
 }
